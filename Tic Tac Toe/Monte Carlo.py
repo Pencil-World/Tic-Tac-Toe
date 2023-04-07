a4089bbd91b51d63664d1556ee4817f0b2104cce
Craner <iayfnp@gmail.com>
2023-04-07 16:39:46 -0700

    porting time!

--- Tic Tac Toe/Monte Carlo.py
+++ Tic Tac Toe/Monte Carlo.py
@@ -13,42 +13,41 @@ def Clear():
 def Print():
     with open('debugger.txt', 'a') as debugger:
         debugger.write("Saving Data\n")
-        debugger.write(f"i: {i}\ntime: {datetime.datetime.now()}\n") # print variables to debugger
-        debugger.write(f"win: {HighScore[0]:.3f}\tloss: {HighScore[1]:.3f}\ttie: {HighScore[2]:.3f}\t\t{R}\n")
+        debugger.write(f"time: {datetime.datetime.now()}\ti: {i}\tbounds: {str(bounds.tolist())}")
 
-# edit
-def Test():
-    print("\nTest")
-    QTable = json.load(open('The One.json', 'r'))
-    for key, val in QTable.items():
-        QTable[key] = np.array(json.loads(val))
+#def Test():
+#    print("\nTest")
+#    QTable = json.load(open('The One.json', 'r'))
+#    for key, val in QTable.items():
+#        QTable[key] = np.array(json.loads(val))
 
-    state = Board()
-    PlayersTurn = True
-    for temp in range(1_000):
-        print('\n' + str(state))
-        action = int(input()) if PlayersTurn else (QTable[repr(state)].argmax() if random.randrange(0, 100) < 95 and repr(state) in QTable else actions[random.randrange(0, actions.shape[0])])
-        state.move(action, 1 if PlayersTurn else 2)
+#    state = Board()
+#    PlayersTurn = True
+#    for temp in range(1_000):
+#        print('\n' + str(state))
+#        action = int(input()) if PlayersTurn else (QTable[repr(state)].argmax() if random.randrange(0, 100) < 95 and repr(state) in QTable else actions[random.randrange(0, actions.shape[0])])
+#        state.move(action, 1 if PlayersTurn else 2)
 
-        actions = state.generate()
-        PlayersTurn = not PlayersTurn
-        if not actions.shape[0] or state.reward == "RED":
-            print('\n' + str(state))
-            state = Board()
-            PlayersTurn = True
+#        actions = state.generate()
+#        PlayersTurn = not PlayersTurn
+#        if not actions.shape[0] or state.reward == "RED":
+#            print('\n' + str(state))
+#            state = Board()
+#            PlayersTurn = True
 
 alpha = 0.00001
-gamma = 0.85
+gamma = 0.5
 episodes = 1_000
 data_size = 1_000
 
+# polynomial regression always converges to a global minimum with MSE
 HighScore = [0, 0, 0]
 R = [10, -10, 0, 0]
-variables = [None] * 2 * 5 # gamma, win reward, lose reward, tie reward, default reward
+bounds = np.zeros([1, 2, 2]) # gamma, win reward, lose reward, tie reward, default reward
 
 #open('The One.json', 'w').write(open('model.json').read())
-#Clear()
-Test()
+Clear()
+#Test()
 
 state = Board()
 lim = 10
@@ -57,11 +56,18 @@ with open('debugger.txt', 'a') as debugger:
 for i in range(data_size):
     if not (i + 1) % 100:
         Print()
-    val = variables[i // 10 % len(variables)]
-    if i // 10 % len(variables):
-        print("hi")
+    it = i / 10 % len(bounds)
+    if it < 0.2:
+        gamma = it == 0.1
+    elif int(it) == 0:
+        gamma = max(0, min((bounds[0][0][0] + bounds[0][1][0]) / 2, 1))
+    #elif it % 1 < 0.2:
+    #    R[int(it) - 1] = 100 * (-1 if it % 1 else 1)
+    #else:
+    #    R[int(it) - 1] = (bounds[int(it)][0][0] + bounds[int(it)][1][0]) / 2
 
-    WinLossRatio = [0, 0, 0]
+    it = int(it)
+    CurrScore = [0, 0, 0]
     QTable = dict()
     for epoch in range(lim):
         for temp in range(episodes):
@@ -74,7 +80,7 @@ for i in range(data_size):
                 action = actions[random.randrange(0, actions.shape[0])]
                 if isModel:
                     mean = QTable.setdefault(repr(state), np.full([9], -1000, dtype = np.float32))
-                    index = mean.argmax() if random.randrange(0, 100) < epoch * 10 else None
+                    index = mean.argmax() if random.randrange(0, 100) < epoch * (100 // lim) else None
                     action = index if index and mean[index] != -1000 else action
                 history.append((action, mean))
                 state.move(action, isModel + 1)
@@ -82,26 +88,23 @@ for i in range(data_size):
                 isModel = not isModel
 
             it = int(isModel) if state.reward == "RED" else 2
-            WinLossRatio[it] += epoch + 1 == lim
+            CurrScore[it] += epoch + 1 == lim
             reward = R[it]
 
-            prev = None
             for action, mean in history[::-1]:
                 isModel = not isModel
                 state.move(action, 0)
                 if isModel:
                     reward = R[3] + gamma * reward
-                    mean[action][prev] = reward if mean[action] == -1000 else mean[action] + alpha * (reward - mean[action]) # if prev is none, set all possibilities of prev to the same value (as if opponent does not play a move)
-                else:
-                    prev = action
+                    mean[action] = reward if mean[action] == -1000 else mean[action] + alpha * (reward - mean[action])
 
-    WinLossRatio /= episodes
-    if HighScore[0] < WinLossRatio[0]:
+    CurrScore /= episodes
+    open('log.txt', 'a').write(f"win rate: {CurrScore[0]:.3f}\tloss rate: {CurrScore[1]:.3f}\ttie rate: {CurrScore[2]:.3f}\n")
+    if HighScore[0] < CurrScore[0]:
         with open('debugger.txt', 'a') as debugger:
-            debugger.write(f"{HighScore[0]:.3f}-{WinLossRatio[0]:.3f}\tgamma: {gamma}\twin reward: {R[0]}\tloss reward: {R[1]}\ttie reward: {R[2]}\n")
-        if HighScore[0] < WinLossRatio[0] or (HighScore[0] == WinLossRatio[0] and HighScore[1] < WinLossRatio[1]):
-            HighScore = WinLossRatio
-            JSON = dict(zip(QTable.keys(), [repr(elem.tolist()) for elem in QTable.values()]))
-            json.dump(JSON, open('model.json', 'w'), indent = 4)
+            debugger.write(f"{HighScore[0]:.3f}-{CurrScore[0]:.3f}\tgamma: {gamma}\twin reward: {R[0]}\tloss reward: {R[1]}\ttie reward: {R[2]}/tdefault reward: {R[3]}\n")
+        HighScore = CurrScore
+        JSON = dict(zip(QTable.keys(), [repr(elem.tolist()) for elem in QTable.values()])) # works for jagged arrays. includes commas
+        json.dump(JSON, open('model.json', 'w'), indent = 4)
 
-    open('log.txt', 'a').write(f"win rate: {WinLossRatio[0]:.3f}\tloss rate: {WinLossRatio[1]:.3f}\ttie rate: {WinLossRatio[2]:.3f}\n")
+        bounds[it][bounds[it][:,1].argmin()] = np.array([gamma if it == 0 else R[it - 1], CurrScore[0]])
--- Tic Tac Toe/Multi-Agent.py
+++ Tic Tac Toe/Multi-Agent.py
@@ -1,138 +1,109 @@
-from Board import Board
-import datetime
-import json
-from tensorflow import keras
-import numpy as np
-import random
+#from Board import Board
+#import datetime
+#import json
+#from tensorflow import keras
+#import numpy as np
+#import random
 
-def Clear():
-    open('buffer.json', 'w').close()
-    open('debugger.txt', 'w').close()
-    open('log.txt', 'w').close()
+#def Clear():
+#    open('buffer.json', 'w').close()
+#    open('debugger.txt', 'w').close()
+#    open('log.txt', 'w').close()
 
-def Load():
-    with open('debugger.txt', 'a') as debugger:
-        debugger.write("Loading Data\n")
-    global i, I_AM_ETERNAL, model, HighScore
+#def Print():
+#    with open('debugger.txt', 'a') as debugger:
+#        debugger.write("Saving Data\n")
+#        debugger.write(f"time: {datetime.datetime.now()}\ti: {i}\tbounds: {str(bounds.tolist())}")
 
-    log = open('debugger.txt', 'r').read().split()
-    epoch = int(log[-log[::-1].index("epoch:")])
-    I_AM_ETERNAL = epoch // data_size
-    i = epoch % data_size
+##def Test():
+##    print("\nTest")
+##    QTable = json.load(open('The One.json', 'r'))
+##    for key, val in QTable.items():
+##        QTable[key] = np.array(json.loads(val))
 
-    JSON = json.load(open('buffer.json', 'r'))
-    length = len(JSON.items())
-    X[:length] = [np.array(json.loads(elem)) for elem in JSON.keys()]
-    Y[:length] = [np.array(json.loads(elem)) for elem in JSON.values()]
+##    state = Board()
+##    PlayersTurn = True
+##    for temp in range(1_000):
+##        print('\n' + str(state))
+##        action = int(input()) if PlayersTurn else (QTable[repr(state)].argmax() if random.randrange(0, 100) < 95 and repr(state) in QTable else actions[random.randrange(0, actions.shape[0])])
+##        state.move(action, 1 if PlayersTurn else 2)
 
-    model = keras.models.load_model('model.h5')
-    HighScore = X[X[:,0].argmax()]
+##        actions = state.generate()
+##        PlayersTurn = not PlayersTurn
+##        if not actions.shape[0] or state.reward == "RED":
+##            print('\n' + str(state))
+##            state = Board()
+##            PlayersTurn = True
 
-def Save():
-    with open('debugger.txt', 'a') as debugger:
-        debugger.write("Saving Data\n")
+#alpha = 0.00001
+#gamma = 0.5
+#episodes = 1_000
+#data_size = 1_000
 
-        JSON = dict(zip([repr(elem.tolist()) for elem in X], [repr(elem.tolist()) for elem in Y]))
-        json.dump(JSON, open('buffer.json', 'w'), indent = 4)
-        model.save('model.h5')
-        debugger.write(f"epoch: {I_AM_ETERNAL * data_size + i}\ntime: {datetime.datetime.now()}\n")
-        debugger.write(f"win: {HighScore[0]:.3f}\tloss: {HighScore[1]:.3f}\ttie: {HighScore[2]:.3f}\t\t{model(np.array([HighScore]))}\n")
+#HighScore = [0, 0, 0]
+#R = [10, -10, 0, 0]
+#bounds = np.zeros([2, 2, 1]) # gamma, win reward, lose reward, tie reward, default reward
 
-def Test():
-    print("\nTest")
-    QTable = json.load(open('The One.json', 'r'))
-    for key, val in QTable.items():
-        QTable[key] = np.array(json.loads(val))
-
-    state = Board()
-    PlayersTurn = True
-    for temp in range(1_000):
-        print('\n' + str(state))
-        action = int(input()) if PlayersTurn else (QTable[repr(state)].argmax() if random.randrange(0, 100) < 95 and repr(state) in QTable else actions[random.randrange(0, actions.shape[0])])
-        state.move(action, 1 if PlayersTurn else 2)
-
-        actions = state.generate()
-        PlayersTurn = not PlayersTurn
-        if not actions.shape[0] or state.reward == "DONE":
-            print('\n' + str(state))
-            state = Board()
-            PlayersTurn = True
-
-alpha = 0.00001
-gamma = 0.85
-episodes = 1_000
-data_size = 1_000
-
-HighScore = [0, 0, 0]
-I_AM_ETERNAL = i = 0
-X = np.zeros([data_size, 3], dtype = np.float32)
-Y = np.zeros([data_size, 3], dtype = np.float32)
-
-model = keras.Sequential([
-        keras.layers.Dense(125, activation = 'relu',
-                            input_shape = [3]),
-        keras.layers.Dense(125, activation = 'relu'),
-        keras.layers.Dense(125, activation = 'relu'),
-        keras.layers.Dense(125, activation = 'relu'),
-        keras.layers.Dense(25, activation = 'relu'),
-        keras.layers.Dense(3)])
-model.compile(optimizer = 'adam', loss = 'mse')
-model.summary()
-
-#open('The One.json', 'w').write(open('model.json').read())
+##open('The One.json', 'w').write(open('model.json').read())
 #Clear()
-#Load()
-Test()
+##Test()
 
-state = Board()
-lim = 100
-with open('debugger.txt', 'a') as debugger:
-    debugger.write(f"start program\n{datetime.datetime.now()}\n")
-for I_AM_ETERNAL in range(I_AM_ETERNAL, 10):
-    for i in range(i, data_size):
-        if not (i + 1) % 100:
-            Save()
-        X[i] = [0, 0, 0]
-        Y[i] = model(np.array([HighScore])) if I_AM_ETERNAL else [random.randint(0, 100), random.randint(-100, 0), random.randint(-50, 50)]
-        QTable = dict()
-        for epoch in range(lim):
-            for temp in range(episodes):
-                history = []
-                isModel = False
-                actions = state.generate()
+#state = Board()
+#lim = 10
+#with open('debugger.txt', 'a') as debugger:
+#    debugger.write(f"start program\n{datetime.datetime.now()}\n")
+#for i in range(data_size):
+#    if not (i + 1) % 100:
+#        Print()
+#    it = bounds[i // 10 % len(bounds)]
+#    if i // 10 % len(bounds):
+#        print("hi")
 
-                while actions.shape[0] and state.reward != "DONE":
-                    mean = None
-                    action = actions[random.randrange(0, actions.shape[0])]
-                    if isModel:
-                        mean = QTable.setdefault(repr(state), np.full([9], -1000, dtype = np.float32))
-                        index = mean.argmax() if random.randrange(0, 100) < min(95, epoch * 100 // 25) else None
-                        action = index if index and mean[index] != -1000 else action
-                    history.append((action, mean))
-                    state.move(action, isModel + 1)
-                    actions = state.generate()
-                    isModel = not isModel
+#    CurrScore = [0, 0, 0]
+#    QTable = dict()
+#    for epoch in range(lim):
+#        for temp in range(episodes):
+#            history = []
+#            actions = state.generate()
 
-                it = int(isModel) if state.reward == "DONE" else 2
-                X[i][it] += lim / 2 <= epoch
-                reward = Y[i][it]
+#            while actions.shape[0] and state.reward != "RED":
+#                strategies = min().argmax()
+#                mean = None
+#                # matrix = np.fromfunction(np.vectorize(lambda i, j: min(mean[i])), [len(mean), 1], dtype = int)
 
-                for action, mean in history[::-1]:
-                    isModel = not isModel
-                    state.move(action, 0)
-                    if isModel:
-                        reward = state.reward + gamma * reward
-                        mean[action] = reward if mean[action] == -1000 else mean[action] + alpha * (reward - mean[action])
+#                action = actions[random.randrange(0, actions.shape[0])]
+#                action = mean[min().argmax()].argmin()
+#                mean = QTable.setdefault(repr(state), np.full([9, 9], -1000, dtype = np.float32))
+#                index = mean.argmax() if random.randrange(0, 100) < epoch * lim else None
+#                action = index if index and mean[index] != -1000 else action
 
-        X[i] /= episodes * lim / 2
-        if 0.99 * HighScore[0] < X[i][0]:
-            with open('debugger.txt', 'a') as debugger:
-                debugger.write(f"{HighScore[0]:.3f}-{X[i][0]:.3f}\twin: {Y[i][0]}\tloss: {Y[i][1]}\ttie: {Y[i][2]}\n")
-            if HighScore[0] < X[i][0] or (HighScore[0] == X[i][0] and HighScore[1] < X[i][1]):
-                HighScore = X[i]
-                JSON = dict(zip(QTable.keys(), [repr(elem.tolist()) for elem in QTable.values()]))
-                json.dump(JSON, open('model.json', 'w'), indent = 4)
+#                history.append((action, mean[action[0]]))
+#                state.move(action, isModel + 1)
+#                actions = state.generate()
 
-        loss = model.fit(X[:None if I_AM_ETERNAL else i + 1], Y[:None if I_AM_ETERNAL else i + 1], epochs = 100, verbose = 0).history['loss'][-1]
-        open('log.txt', 'a').write(f"win: {X[i][0]:.3f}\tloss: {X[i][1]:.3f}\ttie: {X[i][2]:.3f}\tloss: {loss:.3f}\n")
-    i = 0
+#            it = len(history) % 2 if state.reward == "RED" else 2
+#            CurrScore[it] += epoch + 1 == lim
+#            reward = R[it]
+
+#            prev = None
+#            for action, mean in history[::-1]:
+#                isModel = not isModel
+#                state.move(action, 0)
+#                if isModel:
+#                    reward = R[3] + gamma * reward
+#                    if prev:
+#                        mean[action][prev] = reward if mean[action] == -1000 else mean[action] + alpha * (reward - mean[action])
+#                    else:
+#                        mean[action] = np.full([1, 5], reward)
+#                else:
+#                    prev = action
+
+#    CurrScore /= episodes
+#    open('log.txt', 'a').write(f"win rate: {CurrScore[0]:.3f}\tloss rate: {CurrScore[1]:.3f}\ttie rate: {CurrScore[2]:.3f}\n")
+#    if 0.99 * HighScore[0] < CurrScore[0]:
+#        with open('debugger.txt', 'a') as debugger:
+#            debugger.write(f"{HighScore[0]:.3f}-{CurrScore[0]:.3f}\tgamma: {gamma}\twin reward: {R[0]}\tloss reward: {R[1]}\ttie reward: {R[2]}\n")
+#        if HighScore[0] < CurrScore[0]:
+#            HighScore = CurrScore
+#            JSON = dict(zip(QTable.keys(), [repr(elem.tolist()) for elem in QTable.values()])) # works for jagged arrays. includes commas
+#            json.dump(JSON, open('model.json', 'w'), indent = 4)
